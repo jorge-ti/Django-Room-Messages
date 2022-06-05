763f5a6dc3d1d3940f6b5bf720f8618fc32687a3
Jorge Silva da Costa <c00093@comunix.br>
2022-06-05 20:07:05 -0300

    add messsages

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from .views import Home as UrlHome
 from .views import TopicCreate as UrlTopicCreate
 from .views import TopicDetail as UrlTopicDetail
-from .views import RoomCreate as UrlRoom
+from .views import RoomCreate as UrlRoomCreate
+from .views import MessageList as UrlMessageList
 
 
 urlpatterns = [
     path('', UrlHome.as_view(), name='url_home'),
     path('topic/create/', UrlTopicCreate.as_view(), name='url_topic_create'),
     path('topic/detail/<int>', UrlTopicDetail.as_view(), name='url_topic_detail'),
-    path('room/create/', UrlRoom.as_view(), name='url_room_create'),
+    path('room/create/', UrlRoomCreate.as_view(), name='url_room_create'),
+    path('room/messages/<int:pk>', UrlMessageList.as_view(), name='url_message_list'),
 ]
--- apps/base/views.py
+++ apps/base/views.py
@@ -6,7 +6,8 @@ from .forms import RoomForm
 from .forms import TopicForm
 from .models import Topic as TopicModel
 from .models import Room as RoomModel
-#tese
+from .models import Message as MessageModel
+
 
 class Home(View):
     template = 'base/home.html'
@@ -94,3 +95,13 @@ class RoomCreate(View):
         )
         messages.success(request, "Sala cadastrada com sucesso")
         return redirect('url_home')
+
+
+class MessageList(View):
+    template = 'base/message_list.html'
+
+    def get(self, request, *args, **kwargs):
+        id_room = kwargs['pk']
+        messages = MessageModel.objects.filter(room=id_room)
+        context = {'messages':messages}
+        return render(request, self.template, context)
